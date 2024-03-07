30aa7778e6ff42022a8a347101c7fc454e26f464
Murugesan Rengan <rengamurugesan@hotmail.com>
2024-03-07 17:59:29 +0530

    updated the files

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,26 +1,12 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
 
+terraform {
  backend "azurerm" {
     resource_group_name  = "rg-terraform-github-actions-state"
     storage_account_name = "terraformgithubaction"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    
+    key                  = "terraform.tfstate"    
   }
 }
 
-provider "azurerm" {
-  features {}
- 
-}
+
 
--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,24 @@ variable "resource_group_name_prefix" {
   default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
+variable "customer" {
+  type    = string
+  default = "dnug"
+}
+variable "tenantId" {
+  type        = string
+  description = "The id of the tenant to deploy to."
+}
+
+variable "subscriptionId" {
+  type        = string
+  description = "The id of the subscription to deploy to."
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment name (DEV, TEST, PROD). Always pick only the initial letter, i.e. d, t, p"
+}
+variable "clientId" {
+  type = string
+}
