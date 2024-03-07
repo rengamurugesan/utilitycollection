18db5aa02a0d4ddf9f8f1b179ea7c70050898ddc
Murugesan Rengan <rengamurugesan@hotmail.com>
2024-03-07 22:39:54 +0530

    updated

--- infra/providers.tf
+++ infra/providers.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
   client_id                  = var.clientId
   subscription_id            = var.subscriptionId
   tenant_id                  = var.tenantId
+  client_secret              = var.client_secret
   skip_provider_registration = true
   #use_oidc                   = true
  
